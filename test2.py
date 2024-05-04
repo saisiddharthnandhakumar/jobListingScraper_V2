1444472e6ccbfd9d5f2f7a6f8f877d0a59cefb99
saisiddharthnandhakumar <sid@gotdreamsgetbusy.com>
2024-05-04 00:18:04 -0400

    Used the google search API to get data, got meta title, description and link. Now have to use beautifulsoup to parse through HTML code to get the title and description data in the source page. This code works.

--- test2.py
+++ test2.py
@@ -1,21 +1,26 @@
 import googleapiclient.discovery
 
+API_KEY = open('API_KEY').read()
+SEARCH_ENGINE_ID = open('SEARCH_ENGINE_ID').read()
+
 # Create a Google Custom Search API client.
 custom_search_client = googleapiclient.discovery.build(
-    "customsearch", "v1", developerKey="YOUR_API_KEY"
+    "customsearch", "v1", developerKey=API_KEY
 )
 
 # Perform a search.
 search_results = custom_search_client.cse().list(
-    q="YOUR_SEARCH_QUERY", cx="YOUR_SEARCH_ENGINE_ID"
+    q="Registered Nurse Jobs New York City, New York", cx=SEARCH_ENGINE_ID, num=5
 ).execute()
 
 # Get the meta title and meta description of each search result.
 for result in search_results["items"]:
     meta_title = result["title"]
     meta_description = result["snippet"]
+    link = result["link"]
 
     # Print the meta title and meta description.
     print("Meta Title:", meta_title)
     print("Meta Description:", meta_description)
+    print("Link:", link)
     print()
